94948b17b586730cf1f84b52cbc5ba4d4c333751
Lohakare, Sahil (Contractor) <Sahil.Lohakare@transunion.com>
2023-05-22 14:11:52 +0530

    updates done

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -41,8 +41,6 @@ const AddBook = () => {
                     description:"",
                     image:""
                 })
-
-
             } else {
                 navigate("/login")
             }
